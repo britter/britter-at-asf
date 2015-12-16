62ddfa51afc06feee73e07ee1e2ac59a15d2e5d8
Benedikt Ritter <beneritter@gmail.com>
2015-12-16 17:45:38 +0100

    Add jekyll build to upload script

--- upload.sh
+++ upload.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-lftp -u britter,just-for-lftp sftp://home.apache.org << EOF
+function upload_site() {
+  lftp -u britter,just-for-lftp sftp://home.apache.org << EOF
 
-mirror -R _site public_html
+  mirror -R _site public_html
 
-bye
+  bye
 EOF
+}
+
+if jekyll build; then
+  upload_site
+fi
